5315aefa554c71858e0cb3e6d6bdc6e8ac7a04e2
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-05-05 14:10:14 -0300

    Codigo da aula 5

---
+++ codigo-original/aula-5/pagamentos/src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
@@ -0,0 +1,16 @@
+package br.com.alurafood.pagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+
+public class PagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamentosApplication.class, args);
+	}
+
+}
